0f498535b7ff3ea0b64f25cc0e2b7e326a16bbea
brikky <brandon.kerr160@topper.wku.edu>
2016-10-24 11:21:20 -0700

    sets up controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'users/new'
+
+  get 'users/show'
+
+  get 'users/index'
+
+  get 'users/edit'
+
+  get 'posts/new'
+
+  get 'posts/show'
+
+  get 'posts/index'
+
+  get 'posts/edit'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
