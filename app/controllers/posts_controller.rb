541d9536403052f09dfd24e84c24071cf82c5132
brikky <brandon.kerr160@topper.wku.edu>
2016-10-24 16:58:23 -0700

    adds post creation and edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,38 @@
 class PostsController < ApplicationController
   def new
     @post = Post.new
+    @user = current_user
   end
 
   def show
     @post = Post.find(params[:id])
-    @user = User.find(params[:user_id])
+    @user = current_user
   end
 
   def index
       @posts = Post.all
   end
 
+  def create
+    @post = current_user.posts.create(post_params)
+    redirect_to user_post_path(current_user,@post)
+  end
+
   def edit
+    @post = Post.find(params[:id])
+    @user = current_user
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @user = current_user
+    @post.update(post_params)
+    redirect_to user_post_path(@user, @post)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text_body, :photo)
   end
 end
